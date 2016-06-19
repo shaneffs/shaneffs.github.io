d9da9c117d3c10d99b3eca8ab071979d1d00d301
shaneffs <shanef@foxmail.com>
2016-06-19 23:36:24 +0800

    1

---
+++ js/setup.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	
+	$('#cycle').jflickrfeed({
+		limit: 18,
+		qstrings: {
+			id: '132707772@N07'
+		},
+		itemTemplate: '<li><img src="{{image_b}}" alt="{{title}}" /></li>'
+	}, function(data) {
+		$('#cycle div').hide();
+		$('#cycle').cycle({
+            fx:     'fade',
+			timeout: 3000,
+		});
+	});
+    
+    
+
+});
